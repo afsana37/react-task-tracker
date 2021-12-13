f358a72f905abdfb06819a8019dcdecb540bbfea
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-13 14:57:13 +0100

    Button Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-const Header = (props) => {
+import PropTypes from 'prop-types'
+import Button  from './Button'
+
+
+const Header = ({title}) => {
+    const onClick =() => {
+        console.log('Click')
+    }
     return (
-        <header>
-            <h1>{props.title}</h1>
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color='green' text = 'Add' onClick={onClick}/> 
         </header>
     )
 }
@@ -9,4 +17,11 @@ Header.defaultProps = {
     title: 'Task Tracker',
 }
 
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// const headingStyle = {
+//     color: 'red', backgroundColor:'black'
+// }
 export default Header
