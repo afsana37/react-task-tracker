b6a2b480873cb247e1c1296bd2caa7c365637516
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-13 17:56:56 +0100

    Toggle Reminder implemented

--- src/App.js
+++ src/App.js
@@ -36,11 +36,26 @@ function App() {
     id))
   }
 
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder: 
+        !task.reminder} : task
+        )
+    )
+  }
+
   return (
     <div className="container">
       <Header/>    
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete=
-      {deleteTask} /> : 'No Tasks to Show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder} 
+        />
+      ) : (
+        'No Tasks to Show'
+      )}
     </div>
   );
 }
