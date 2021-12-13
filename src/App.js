46fe725cc88f1e1a711f2f778004232035b73042
Afsana <afsana.abida@stud.uni-bamberg.de>
2021-12-13 16:09:58 +0100

    Tasks added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Zoom meeting',
+        day: 'Dec 15th at 11:00 am',
+        reminder:true,
+    },
+
+    {
+        id: 2,
+        text: 'Food Shopping',
+        day:  'Dec 15th at 12:00 pm',
+        reminder:false,
+    },
+
+    {
+        id: 3,
+        text: 'Assignment Submission',
+        day:  'Dec 15th at 2:30 pm',
+        reminder:false,
+    },
+
+ 
+]
+   )
+  
   return (
     <div className="container">
       <Header/>    
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
